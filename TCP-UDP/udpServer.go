49b839d4e9e6523b500e70ce29b9b76a982218fe
MohamedMohsen300 <momohsenmomohsen95@gmail.com>
2025-09-20 20:49:47 +0300

    Update udpServer.go

--- TCP-UDP/udpServer.go
+++ TCP-UDP/udpServer.go
@@ -45,8 +45,9 @@ func udp() {
 	// to send check after 10 min
 	go func() {
 		for {
-			time.Sleep(10 * time.Minute)
+			time.Sleep(4 * time.Minute)
 			for _, cAddr := range clientsAddress {
+				fmt.Println("send conn")
 				conn.WriteTo([]byte("check connection"), cAddr)
 			}
 		}
