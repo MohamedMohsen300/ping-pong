f67cad208697b924a739dcb6d8a9d2c842ddb611
Mohamed Mohsen <momohsenmomohsen95@gmail.com>
2025-09-23 15:06:10 +0300

    udpserver

--- udpServer.go
+++ udpServer.go
@@ -71,7 +71,7 @@ func (s *Server) HandleMessage(addr *net.UDPAddr, message string) {
 }
 
 func (s *Server) checkConnection() {
-	ticker := time.NewTicker(time.Minute)
+	ticker := time.NewTicker(10*time.Minute)
 	for {
 		<-ticker.C
 		s.mu.Lock()
