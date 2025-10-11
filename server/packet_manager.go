86ce20ff83b8ff42205d19cd17be377f8fee6e42
Mohamed Mohsen <momohsenmomohsen95@gmail.com>
2025-10-11 16:18:03 +0300

    udp_update

--- server/packet_manager.go
+++ server/packet_manager.go
@@ -73,7 +73,7 @@ func (s *Server) PacketParser(addr *net.UDPAddr, packet []byte) {
 }
 
 func (s *Server) fieldPacketTrackingWorker() {
-	ticker := time.NewTicker(800 * time.Millisecond)
+	ticker := time.NewTicker(1 * time.Second)
 	defer ticker.Stop()
 
 	for range ticker.C {
@@ -85,7 +85,7 @@ func (s *Server) fieldPacketTrackingWorker() {
 
 		for packetID, pending := range pendings {
 			if now.Sub(pending.LastSend) >= 500*time.Millisecond {
-				// fmt.Printf("Retransmitting packet %d\n", packetID)
+				fmt.Printf("Retransmitting packet %d\n", packetID)
 				s.muxPending <- models.Mutex{Action: "updatePending", PacketID: packetID}
 			}
 			// time.Sleep(20 * time.Millisecond)
