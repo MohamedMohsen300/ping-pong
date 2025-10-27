f9a9bb97685f4a1ae98076ed3ba892aebd56f6a2
Mohamed Mohsen <momohsenmomohsen95@gmail.com>
2025-10-27 13:09:11 +0300

    udp_update

--- server/udpServer.go
+++ server/udpServer.go
@@ -442,7 +442,7 @@ func (s *Server) SendFileToClient(client *Client, filepath string, filename stri
 		copy(payload[4:], chunkData)
 
 		s.packetGenerator(client.Addr, Chunk, payload, 0, nil)
-		time.Sleep(500 * time.Millisecond)
+		time.Sleep(100 * time.Millisecond)
 	}
 	return nil
 }
