243e2abb87f7ecc13b1ad02d8588b0550814565e
Mohamed Mohsen <momohsenmomohsen95@gmail.com>
2025-11-03 10:55:18 +0200

    udp_update

--- server/udpServer.go
+++ server/udpServer.go
@@ -291,7 +291,7 @@ func stdinCommandLoop() {
 }
 
 func main() {
-	addr := ":11000"
+	addr := ":9000"
 	// quicConfig := &quic.Config{
 	// 	KeepAlivePeriod: 10 * time.Second,
 	// 	MaxIdleTimeout:  2 * time.Minute,
